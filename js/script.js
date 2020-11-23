7d5ca60e63a2c5ff6e8f064d358fd7eba494e15b
Entity-Code <mattia.tummolilllo@gmail.com>
2020-11-23 18:43:25 +0100

    Completamento milestone2

--- js/script.js
+++ js/script.js
@@ -63,6 +63,16 @@ var app = new Vue({
                 testo: "MESSAGGIO DEL BOT ok",
                 data: "27/10/2020 11:14:52",
                 mioMessaggio: false
+              },
+              {
+                testo: "MESSAGGIO DEL BOT ok",
+                data: "27/10/2020 11:14:52",
+                mioMessaggio: false
+              },
+              {
+                testo: "MIO MESSAGGIO non mi è piaciuto il tuo comportamento ieri",
+                data: "26/10/2020 7:11:43",
+                mioMessaggio: true
               }
             ]
          },
@@ -73,6 +83,21 @@ var app = new Vue({
             msg: "Luisa? baff...",
             img:"img/avatar_5.jpg",
             messaggi: [
+              {
+                testo: "MIO MESSAGGIO sei tornato a casa? fammi sapere",
+                data: "23/11/2020 17:11:43",
+                mioMessaggio: true
+              },
+              {
+                testo: "MESSAGGIO DEL BOT si sono tornata da poco, tu?",
+                data: "23/11/2020 18:11:51",
+                mioMessaggio: false
+              },
+              {
+                testo: "MIO MESSAGGIO sei tornato a casa? fammi sapere",
+                data: "23/11/2020 17:11:43",
+                mioMessaggio: true
+              },
               {
                 testo: "MIO MESSAGGIO sei tornato a casa? fammi sapere",
                 data: "23/11/2020 17:11:43",
@@ -101,6 +126,21 @@ var app = new Vue({
                 testo: "MESSAGGIO DEL BOT solo se te lo meriti",
                 data: "9/9/2020 10:32:43",
                 mioMessaggio: false
+              },
+              {
+                testo: "MESSAGGIO DEL BOT solo se te lo meriti",
+                data: "9/9/2020 10:32:43",
+                mioMessaggio: false
+              },
+              {
+                testo: "MESSAGGIO DEL BOT solo se te lo meriti",
+                data: "9/9/2020 10:32:43",
+                mioMessaggio: false
+              },
+              {
+                testo: "MIO MESSAGGIO papà mi compro il nuovo cellulare?",
+                data: "23/11/2020 10:34:21",
+                mioMessaggio: true
               }
             ]
          }
